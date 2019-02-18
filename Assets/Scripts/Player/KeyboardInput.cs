66942f3d8772ddd9c1955704605afda3da65d881
Sjors Scholten <sjors.scholten@hotmail.com>
2019-02-18 10:37:38 +0100

    edited and added scripts
    interactable, keyboard input, playercontroller, timer

---
+++ Assets/Scripts/Player/KeyboardInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour, IInput {
+    
+    public event EventHandler Interacting;
+
+    private void Update() {
+        if (Input.GetButtonDown("Interaction")) {
+            OnInteract();
+        }
+    }
+
+    private void OnInteract() {
+        Interacting?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector3 MovementDirection {
+        get {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            return new Vector3(horizontal, 0, vertical);
+        }
+    }
+
+    public bool IsRunning => Input.GetButton("Running");
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private CharacterController character;
+    private IInput input;
+
+    private void Awake() {
+        throw new System.NotImplementedException();
+    }
+    
+    
+}
