2017b03b8af959873af83f2c37d9e9b9d69baa39
Sjors Scholten <scholtensjors22@gmail.com>
2019-02-19 19:18:58 +0100

    created simple timer

---
+++ Assets/Scripts/Timer/SimpleTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTimer : MonoBehaviour, ITimer {
+
+    [SerializeField]
+    private float initTime = 10;
+    
+    public float CurrentTime { get; private set; }
+    public bool IsActive { get; private set; }
+    
+    public event EventHandler OnTimerElapse;
+
+    private void Update() {
+        if (!IsActive) return;
+        if (CurrentTime <= 0) {
+            Stop();
+            return;
+        }
+        CurrentTime -= Time.deltaTime;
+    }
+
+    public void Initialize() {
+        CurrentTime = initTime;
+        IsActive = true;
+    }
+
+    public void Stop() {
+        IsActive = false;
+        CurrentTime = 0;
+    }
+
+    public float StartTime => initTime;
+}
