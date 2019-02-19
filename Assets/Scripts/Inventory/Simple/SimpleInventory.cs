9b3fd519afe16a89cf8181da4e671b096e697833
Sjors Scholten <scholtensjors22@gmail.com>
2019-02-19 19:20:44 +0100

    added a simple inventory

---
+++ Assets/Scripts/Inventory/Simple/SimpleInventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleInventory : MonoBehaviour, IInventory {
+    
+    private readonly List<IItem> items = new List<IItem>(); 
+    
+    public void AddItem(IItem item) {
+        items.Add(item);
+    }
+
+    public void RemoveItem(IItem item) {
+        items.Remove(item);
+    }
+
+    public IItem GetItemWithId(int id) {
+        var item = items.Find(x => x.Id.Equals(id));
+        return item;
+    }
+}
