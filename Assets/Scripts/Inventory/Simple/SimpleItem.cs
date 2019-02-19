598326dcc185d0f89ca7cd7412319baaaf3c418f
Sjors Scholten <scholtensjors22@gmail.com>
2019-02-19 19:20:30 +0100

    added simple item

---
+++ Assets/Scripts/Inventory/Simple/SimpleItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+[CreateAssetMenu(menuName = "items", fileName = "simple item")]
+public class SimpleItem : ScriptableObject, IItem {
+    
+    [SerializeField] 
+    private int id = -1;
+
+    [SerializeField]
+    private new string name = "default simple object";
+
+    public int Id => id;
+    public string Name => name;
+}
